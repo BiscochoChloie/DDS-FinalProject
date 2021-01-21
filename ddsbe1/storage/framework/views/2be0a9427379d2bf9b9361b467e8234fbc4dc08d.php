d30b3756e5c517afcb6cd2e95c134e59264d18f1
BiscochoChloie <58418206+BiscochoChloie@users.noreply.github.com>
2021-01-22 02:27:53 +0800

    Add files via upload

---
+++ ddsbe1/storage/framework/views/2be0a9427379d2bf9b9361b467e8234fbc4dc08d.php
@@ -0,0 +1,48 @@
+<?php $__env->startSection('content'); ?>
+    <hr>	
+		<h1 class="text-center">Roles</h1>	
+	<hr> 
+
+	<a href="<?php echo e(route('roles.create')); ?>" class="btn btn-primary">Create</a>
+		
+	<table class= "table table-hover">
+		<thead>		
+			<th>Name</th>
+			<th>Department</th>
+			<th>Salary</th>
+			<th>Edit</th>
+			<th>Trash</th>
+		</thead>	
+			
+		<tbody>
+			<?php if($roles->count()> 0): ?>
+				<?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $role): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<tr>						
+						<td><a href="<?php echo e(route('roles.show', ['slug' => $role->slug])); ?>" ><?php echo e($role->name); ?></a></td>						
+						
+						<td><?php echo e($role->department->name); ?></td>
+						<td><?php echo e($role->salary); ?></td>	
+						<td>
+							<a href="<?php echo e(route('roles.edit', ['id' => $role->id])); ?>" class="btn btn-info">Edit</a>
+						</td>
+						<td>
+							<form action="<?php echo e(route('roles.destroy', ['id' => $role->id])); ?>" method="POST">
+								<?php echo e(csrf_field()); ?>
+
+								<?php echo e(method_field('DELETE')); ?>
+
+								<button class="btn btn-danger">Bin</button>
+							</form>
+						</td>
+					</tr>
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+			<?php else: ?>
+				<tr> 
+					<th colspan="5" class="text-center">Empty</th>
+				</tr>
+			<?php endif; ?>
+		</tbody>
+	</table>
+	<div class="text-center"><?php echo e($roles->links()); ?></div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ ddsbe1/storage/framework/views/e527056027537e7c2da9f473e9b8c2783b9d17aa.php
@@ -0,0 +1,34 @@
+<?php $__env->startSection('content'); ?>
+    <div class="col-lg-12">
+		<h1 class="page-header">Department: <?php echo e($department->name); ?></h1>
+	</div>
+		
+
+	<table class= "table table-hover">
+		<thead>
+			<th>Role</th>
+			<th>Salary</th>
+		</thead>
+		
+		<tbody>
+			<?php if($department->roles->count() > 0): ?>
+				<?php $__currentLoopData = $department->roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $role): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<tr>
+						<td>
+							<a href="<?php echo e(route('roles.show', ['slug'=>$role->slug])); ?>"><?php echo e($role->name); ?></a>
+						</td>
+						<td><?php echo e($role->salary); ?></td>
+					</tr>
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+			<?php else: ?>
+				<tr> 
+					<th colspan="5" class="text-center">No Roles assigned in this department yet</th>
+				</tr>
+			<?php endif; ?>
+		
+		</tbody>
+	
+	</table>
+		
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
